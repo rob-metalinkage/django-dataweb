878ce5f2d0b1c80a4b9c309d76173dfa7f63da3f
Rob Atkinson <rob@metalinkage.com.au>
2016-09-14 18:09:56 +1000

    SKOS entailment SPIN rules and RDF_IO for coded dimensions - more QB4ST support to come..

--- dataweb/fixtures/qb4st_dim.py
+++ dataweb/fixtures/qb4st_dim.py
@@ -1,5 +1,5 @@
 from dataweb.models import *
-from rdf_io.models import Namespace, ObjectMapping,AttributeMapping 
+from rdf_io.models import Namespace, ObjectType,ObjectMapping,AttributeMapping 
 from django.contrib.contenttypes.models import ContentType
 from skosxl.models import Scheme
 
@@ -38,6 +38,41 @@ def load_rdf_mappings():
     """
         load base QB components
     """
-    print "loading RDF_IO mappings"
 
+    (object_type,created) = ObjectType.objects.get_or_create(uri="qb:DimensionProperty", defaults = { "label" : "RDF Datacube DimensionProperty" })
+    
+    pm = commondim(object_type, "QBDimension", "QB Dimension Common Elements" )
+    # specific mapping
+    # am = AttributeMapping(scope=pm, attr="collection", predicate="prof:collection", is_resource=True).save()
+
+    (object_type,created) = ObjectType.objects.get_or_create(uri="qb:CodedDimensionProperty", defaults = { "label" : "RDF Datacube Coded DimensionProperty" })
+    pm = commondim(object_type, "QBCodedDimension", "QB Coded Dimension Specific Elements" )
+    am = AttributeMapping(scope=pm, attr="codelist.uri", predicate="qb:codelist", is_resource=True).save()
+
+    #(object_type,created) = ObjectType.objects.get_or_create(uri="qb4st:CoordDimension", defaults = { "label" : "RDF Datacube Spatial Coordinate DimensionProperty" })
+    #pm = commondim(object_type, "QBSpatialDimension", "QB Spatial Dimension Specific Elements" )
+    #am = AttributeMapping(scope=pm, attr="crs", predicate="qb:codelist", is_resource=True).save()
+ 
+def commondim(object_type,content_type_label, title):
+    content_type = ContentType.objects.get(app_label="dataweb",model=content_type_label.lower())
+ 
+    (pm,created) =   ObjectMapping.objects.get_or_create(name=title, defaults =
+        { "auto_push" : True , 
+          "id_attr" : "uri",
+          "target_uri_expr" : "uri",
+          "content_type" : content_type
+        })
+    if not created :
+        AttributeMapping.objects.filter(scope=pm).delete()
+    
+    pm.obj_type.add(object_type)
+        
+    am = AttributeMapping(scope=pm, attr="comment", predicate="rdfs:comment", is_resource=False).save()
+    am = AttributeMapping(scope=pm, attr="label", predicate="rdfs:label", is_resource=False).save()
+    am = AttributeMapping(scope=pm, attr="comment", predicate="rdfs:comment", is_resource=False).save()
+    am = AttributeMapping(scope=pm, attr="helptext", predicate="rdfs:comment", is_resource=False).save()
+    am = AttributeMapping(scope=pm, attr="range", predicate="rdfs:range", is_resource=True).save()
+    am = AttributeMapping(scope=pm, attr="concept", predicate="qb:concept", is_resource=True).save()
+    am = AttributeMapping(scope=pm, attr="sub_type_of.uri", predicate="rdfs:subPropertyOf", is_resource=True).save()
+    return pm   
 
